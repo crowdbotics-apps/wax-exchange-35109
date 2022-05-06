ebf2096e825d53a2a1b76c1d846371096f8e8ee4
Crowdbotics <team@crowdbotics.com>
2022-05-06 03:45:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wax_exchange_35109",
+  url: "https://wax_exchange_35109.botics.co",
+  api: "https://wax_exchange_35109.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
